221b9c2c4a920f53dc3c9ed1553f20d57a9f69dc
khalidhub7 <louguikhalid7@gmail.com>
2024-02-10 09:31:23 -0800

    up

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python3
 """
-file_storage that manages
-our storage
+file_storage that manages storage
 """
 
 import json
+import os
 from models.base_model import BaseModel
 
 
 class FileStorage:
     """
-    define a class
-    FileStorage that manage objects storage
-    attributes:
-        __file_path (str): file storage path
-        __objects (dict): dictionary of object created
+    define a classFileStorage
+    
+    __file_path: file storage
+    __objects: dictionary of object created
     """
-
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects = {}
+    storage = 0
 
     def all(self):
-        """ get objects of the class """
-        return FileStorage.__objects
+        """
+        get objects of the class
+        """
+        return self.__objects.to_dict()
 
     def new(self, obj):
-        """ Add new object to objects dictionary """
-        FileStorage.__objects["{}.{}\
-".format(obj.to_dict()['__class__'], obj.id)] = obj
+        """
+        Add new object to objects dictionary
+        """
+        key = "{}.id".format(obj.__class__.__name__) # ymkn ndir assign l id b self.id
+        self.__objects[key] = obj
 
     def save(self):
-        """ Save objects to json file """
-        dictionary = {}
-        for key in FileStorage.__objects:
-            dictionary[key] = FileStorage.__objects[key].to_dict()
-        with open(FileStorage.__file_path, "w") as file:
-            file.write(json.dumps(dictionary))
+        """
+        save objects to json file
+        """
+        with open(self.__file_path, 'w') as file:
+            json.dump(self.__objects, file)
 
     def reload(self):
-        """ load objects from json file """
-        try:
-            with open(FileStorage.__file_path, "r") as file:
-                dictionary = json.loads(file.read())
-            for key in dictionary:
-                self.new(eval(dictionary[key]["__class__"])(**dictionary[key]))
-        except IOError:
-            pass
+        """
+        load objects from json file
+        """
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                self.__objects = json.load(file)
