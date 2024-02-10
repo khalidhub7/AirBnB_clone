ba83bc52be80d2e8b5fef2dd8f4ba21abac46b8b
khalidhub7 <louguikhalid7@gmail.com>
2024-02-10 09:02:24 -0800

    up

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@ base model parent of other classes
 
 from datetime import datetime
 import uuid
-import models
 
 
 class BaseModel:
@@ -14,7 +13,7 @@ class BaseModel:
             BaseModel class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize the `BaseModel` object
 
@@ -26,6 +25,18 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for i in kwargs:
+                if i == "__class__":
+                    pass
+                elif i == "id":
+                    self.id = kwargs[i]
+                elif i == "created_at":
+                    self.created_at = kwargs[i]
+                elif i == "updated_at":
+                    self.updated_at = kwargs[i]
+                else:
+                    setattr(self, i, kwargs[i])
 
     def __str__(self):
         """
