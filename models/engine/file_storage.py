954c1ff2d91533ac5daf6612bccca98d60369a8f
khalidhub7 <louguikhalid7@gmail.com>
2024-02-10 14:02:36 -0800

    up

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,11 +35,12 @@ class FileStorage:
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        save objects to json file
-        """
-        with open(self.__file_path, 'w') as file:
-            json.dump(self.__objects, file)
+        """ Save objects to json file """
+        dictionary = {}
+        for key in FileStorage.__objects:
+            dictionary[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, "w") as file:
+            file.write(json.dumps(dictionary))
 
     def reload(self):
         """
