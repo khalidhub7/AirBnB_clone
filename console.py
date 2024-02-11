7e09c3dccd3a22d433be0d33b21ca83aa34aac2d
khalidhub7 <louguikhalid7@gmail.com>
2024-02-10 16:45:12 -0800

    up

--- console.py
+++ console.py
@@ -15,5 +15,17 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
+    def do_create(self, arg):
+        """command that create new instance"""
+        name_of_class = arg.split()
+        if len(name_of_class) == 0:
+            print("** class name missing **")
+        elif name_of_class[0] not in HBNBCommand.list_classess:
+            print("** class doesn't exist **")
+        else:
+            new = eval(name_of_class[0])()
+            models.storage.save()
+            print(new.id)
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
