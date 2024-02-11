b6c0afab727f3750190272991fc75745ad4df364
khalidhub7 <louguikhalid7@gmail.com>
2024-02-10 18:54:34 -0800

    up

--- models/base_model.py
+++ models/base_model.py
@@ -42,6 +42,10 @@ class BaseModel:
 %Y-%m-%dT%H:%M:%S.%f")
                 else:
                     setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
