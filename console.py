b3489ed89a617bf2baf74bfb9d01e24d842fa16b
khalidhub7 <louguikhalid7@gmail.com>
2024-02-12 16:38:21 -0800

    up

--- console.py
+++ console.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-'''AirBnB_ console'''
+'''
+AirBnB_ console'''
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    '''define HBNBCommand class'''
+    '''
+    define HBNBCommand class'''
 
     prompt = '(hbnb)'
 
-    def do_EOF(self):
-        '''EOF command'''
+    def do_EOF(self, line):
+        '''
+        EOF command'''
         return True
 
-    def do_quit(self):
-        '''Quit command to exit the program'''
+    def do_quit(self, line):
+        '''
+        Quit command to exit the program'''
         return True
 
     def emptyline(self):
-        '''emptyline haldle'''
+        '''
+        emptyline haldle'''
         pass
 
 if __name__ == '__main__':
