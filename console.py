9f607dc47421c4a862234282fa6657039661e0a6
khalidhub7 <louguikhalid7@gmail.com>
2024-02-16 20:33:22 -0800

    up

--- console.py
+++ console.py
@@ -87,8 +87,8 @@ class HBNBCommand(cmd.Cmd):
         elif myargs[0]+'.'+myargs[1] not in storage.all():
             print('** no instance found **')
             return False
-        print(storage.all()["{}.{}".format(myargs[0], myargs[1])])
         return True
+        print(storage.all()["{}.{}".format(myargs[0], myargs[1])])
 
     def do_destroy(self, arg):
         '''destroy command that destroy object'''
