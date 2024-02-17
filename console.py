03252fc19a6bf8fcef0b4093e5f694a3c4571663
khalidhub7 <louguikhalid7@gmail.com>
2024-02-16 21:31:38 -0800

    up

--- console.py
+++ console.py
@@ -46,19 +46,19 @@ class HBNBCommand(cmd.Cmd):
         '''
         check if class name and id exist
         '''
-        args_list = shlex.split(arg)
-        if len(args_list) == 0:
-            print("** class name missing **")
+        myargs = shlex.split(arg)
+        if len(myargs) == 0:
+            print('** class name missing **')
             return False
-        if args_list[0] not in HBNBCommand.list_classess:
+        if myargs[0] not in HBNBCommand.classes_list:
             print("** class doesn't exist **")
             return False
-        if len(args_list) < 2:
-            print("** instance id missing **")
+        if len(myargs) < 2:
+            print('** instance id missing **')
             return False
-        if args_list[0]+"."+args_list[1] in storage.all():
+        if myargs[0]+"."+myargs[1] in storage.all():
             return True
-        print("** no instance found **")
+        print('** no instance found **')
 
     def check_attr_exist(self, arg):
         '''
