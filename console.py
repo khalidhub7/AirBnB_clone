56002f070c18ec472ab5ea45da8ff8fce40aab52
khalidhub7 <louguikhalid7@gmail.com>
2024-02-16 20:02:50 -0800

    up

--- console.py
+++ console.py
@@ -74,11 +74,16 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_show(self, arg):
-        '''show command to prints object representation'''
-        myargs = shlex.split(arg)
-        if self.check_id_exist(arg):
-            print(storage.all()["{}.{}\
-".format(myargs[0], myargs[1])])
+        """show command to prints object representation"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print('** class name missing **')
+        elif args[0] not in HBNBCommand.list_classess:
+            print('** class doesn\'t exist **')
+        elif args[0] + '.' + args[1] in storage.all():
+            print(storage.all()["{}.{}".format(args[0], args[1])])
+        else:
+            print('** no instance found **')
 
     def do_destroy(self, arg):
         '''destroy command that destroy object'''
